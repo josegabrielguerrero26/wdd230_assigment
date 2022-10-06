04de5f71044c51940726cc01fb0663fca8d7d0a5
Gabe G <97242028+josegabrielguerrero26@users.noreply.github.com>
2022-10-05 19:09:31 -0500

    changes header

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -1,7 +1,14 @@
-function toggleMenu() {
+/*function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("open");
   document.getElementById("hamburgerBtn").classList.toggle("open");
 }
 
 const x = document.getElementById("hamburgerBtn")
 x.onclick = toggleMenu;
+*/
+try {
+  const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+  document.getElementById('currentdate').textContent = new Date(). toLocaleDateString('en-US', options);   
+} catch (e) {
+  alert('Error with code or your browser does not support Locate');
+}
