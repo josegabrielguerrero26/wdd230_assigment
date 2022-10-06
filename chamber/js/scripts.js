31b7df11c3d5a4e8a778fde432c89f49632ea5a1
Gabe G <97242028+josegabrielguerrero26@users.noreply.github.com>
2022-10-06 13:16:06 -0500

    fixes

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -1,11 +1,12 @@
-/*function toggleMenu() {
+function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("open");
   document.getElementById("hamburgerBtn").classList.toggle("open");
 }
 
 const x = document.getElementById("hamburgerBtn")
 x.onclick = toggleMenu;
-*/
+
+
 try {
   const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
   document.getElementById('currentdate').textContent = new Date(). toLocaleDateString('en-US', options);   
