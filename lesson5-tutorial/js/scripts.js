aa2c2fcb6cfa0dad6a3e39de53d9664f181299a1
Gabe G <97242028+josegabrielguerrero26@users.noreply.github.com>
2022-10-11 12:19:41 -0500

    done practice

--- lesson5-tutorial/js/scripts.js
+++ lesson5-tutorial/js/scripts.js
@@ -1,17 +1,24 @@
 document.getElementById("mybtn").addEventListener("click",function (){
     let mycup = document.getElementById("theitem").value;
     console.log(mycup)
+
     if (mycup!=="") {
+        //create button X
         const newBTN = document.createElement("button");
         newBTN.innerHTML="\u274C";
 
+        // create a list with the lli
         const newLI= document.createElement("li");
         newLI.textContent=mycup;
         newLI.appendChild(newBTN);
-
         document.getElementById("favs").appendChild(newLI);
-
-        document.getElementById("theitem").value=""
+        document.getElementById("theitem").value="";
+        
+        //remove element with X buttom
+        newBTN.addEventListener('click', function () {
+        document.getElementById("favs").removeChild(newLI);
+        newLI.removeChild(newBTN)});
+            
     }// end of if
 
 });
