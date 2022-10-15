3ca0efe114468389a44f29ae10f12fc6be4b72a8
Gabe G <97242028+josegabrielguerrero26@users.noreply.github.com>
2022-10-14 19:57:38 -0500

    done

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -19,3 +19,21 @@ let fullDate = lastModified.toLocaleString('en-US', {month: "2-digit", day: "2-d
 let time = lastModified.toLocaleString('en-GB', {hour: "2-digit", minute: "2-digit", second: "2-digit"});
 let dateTime = `Last Updated: ${fullDate} ${time}`;
 document.querySelector("#lastModified").textContent= dateTime;
+
+//ad buttom 
+const today= new Date();
+console.log(today);
+
+  const dayNumber = today.getDay();
+  console.log(dayNumber);
+
+  const element=document.getElementById("joinus");
+
+  if (dayNumber==1){
+    element.classList.add("showme");
+  }
+  else if(dayNumber==2){
+    element.classList.add("showme");
+  }else{
+    element.classList.add("hide");
+  }
