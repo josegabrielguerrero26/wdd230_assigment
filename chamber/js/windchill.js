babf80a476176e1b17778d9d1b99e40702276de5
Gabe G <97242028+josegabrielguerrero26@users.noreply.github.com>
2022-10-19 19:11:12 -0500

    windchill 1-2

---
+++ chamber/js/windchill.js
@@ -0,0 +1,5 @@
+const tempNumber=parseFloat(document.getElementById("temp").textContent);
+console.log(tempNumber);
+
+const speedNumber=parseFloat(document.getElementById("speed").textContent);
+console.log(speedNumber);
