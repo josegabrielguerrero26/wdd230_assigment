ffe1584c8d5d7f3cf41b2d414f87dd349787984e
Gabe G <97242028+josegabrielguerrero26@users.noreply.github.com>
2022-11-09 13:50:04 -0500

    fixes

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -9,11 +9,4 @@ const x = document.getElementById("hamburgerBtn")
 x.onclick = toggleMenu;
 
 
-try {
-  const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-  document.getElementById('currentdate').textContent = new Date(). toLocaleDateString('en-US', options);   
-} catch (e) {
-  alert('Error with code or your browser does not support Locate');
-}
-
 
