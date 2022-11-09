b70349573ec7881ac807cbf9ff0161944aa7270d
Gabe G <97242028+josegabrielguerrero26@users.noreply.github.com>
2022-11-09 13:16:57 -0500

    chenges

--- chamber/js/adv.js
+++ chamber/js/adv.js
@@ -1,4 +1,11 @@
 //ad buttom 
+
+let lastModified = new Date(document.lastModified);
+let fullDate = lastModified.toLocaleString('en-US', {month: "2-digit", day: "2-digit", year: "numeric"});
+let time = lastModified.toLocaleString('en-GB', {hour: "2-digit", minute: "2-digit", second: "2-digit"});
+let dateTime = `Last Updated: ${fullDate} ${time}`;
+document.querySelector("#lastModified").textContent= dateTime;
+
 const today= new Date();
 console.log(today);
 
--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -16,9 +16,4 @@ try {
   alert('Error with code or your browser does not support Locate');
 }
 
-let lastModified = new Date(document.lastModified);
-let fullDate = lastModified.toLocaleString('en-US', {month: "2-digit", day: "2-digit", year: "numeric"});
-let time = lastModified.toLocaleString('en-GB', {hour: "2-digit", minute: "2-digit", second: "2-digit"});
-let dateTime = `Last Updated: ${fullDate} ${time}`;
-document.querySelector("#lastModified").textContent= dateTime;
 
