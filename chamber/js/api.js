585c84c073fbb16a755c40ad43e415f9261b1f18
Gabe G <97242028+josegabrielguerrero26@users.noreply.github.com>
2022-11-10 13:51:22 -0500

    small, medium

--- chamber/js/api.js
+++ chamber/js/api.js
@@ -6,13 +6,15 @@ const business = companies;
 for (let i = 0; i < business.length; i++) {
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
+    let h1 = document.createElement('p');
     let ceo = document.createElement('p');
     let mainb = document.createElement('p');
     let ratings=document.createElement('p');
     let image = document.createElement('img');
 
     image.setAttribute('src', business[i].filepath);
-    h2.textContent = business[i].name + ' ' + business[i].numberofemployees;
+    h2.textContent =  ' Name: ' + business[i].name ;
+    h1.textContent='Headcount: ' + business[i].numberofemployees;
     ceo.textContent = 'CEO Name: ' + business[i].ceo;
     mainb.textContent = 'Industry: ' + business[i].mainbusiness;
     ratings.textContent='Ratings: ' + business[i].ratings;
@@ -20,6 +22,7 @@ for (let i = 0; i < business.length; i++) {
 
 
     card.appendChild(h2);
+    card.appendChild(h1);
     card.appendChild(ceo);
     card.appendChild(mainb);
     card.appendChild(ratings);
