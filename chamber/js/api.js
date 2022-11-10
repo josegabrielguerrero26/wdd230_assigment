5c2e5c6677a87bf154145fa5eb5558701fbb73c0
Gabe G <97242028+josegabrielguerrero26@users.noreply.github.com>
2022-11-09 19:07:59 -0500

    api htm, css small done

--- chamber/js/api.js
+++ chamber/js/api.js
@@ -1,4 +1,28 @@
-console.log("hello");
-import companies from './JSON/dire.json' assert{type: 'json'};
 
-console.log(companies)
+import companies from '../JSON/dire.json' assert{type: 'json'};
+console.log(companies[0].ceo, companies[3].name)
+
+const business = companies;
+for (let i = 0; i < business.length; i++) {
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let ceo = document.createElement('p');
+    let mainb = document.createElement('p');
+    let ratings=document.createElement('p');
+    let image = document.createElement('img');
+
+    image.setAttribute('src', business[i].filepath);
+    h2.textContent = business[i].name + ' ' + business[i].numberofemployees;
+    ceo.textContent = 'CEO Name: ' + business[i].ceo;
+    mainb.textContent = 'Industry: ' + business[i].mainbusiness;
+    ratings.textContent='Ratings: ' + business[i].ratings;
+    image.setAttribute('loading', 'lazy');
+
+
+    card.appendChild(h2);
+    card.appendChild(ceo);
+    card.appendChild(mainb);
+    card.appendChild(ratings);
+    card.appendChild(image);
+    document.querySelector('div.cards').appendChild(card);
+} ;
