6e3e1946a1daa937eea40e58e8a3d39d45da5a85
Gabe G <97242028+josegabrielguerrero26@users.noreply.github.com>
2022-11-17 17:18:21 -0500

    working weather

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,7 +1,7 @@
-const tempNumber=parseFloat(document.getElementById("temp").textContent);
+const tempNumber=parseFloat(document.getElementById("currentTemp").textContent);
 //console.log(tempNumber);
 
-const speedNumber=parseFloat(document.getElementById("speed").textContent);
+const speedNumber=parseFloat(document.getElementById("windSpeed").textContent);
 //console.log(speedNumber);
 
 let windchill = 35.74 +(0.6215* tempNumber)-(35.75*Math.pow(speedNumber,0.16))+(0.4275*tempNumber*Math.pow(speedNumber, 0.16));
