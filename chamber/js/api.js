d35e10c3f79a3c9c78b062cbdb234424840f3d5f
Gabe G <97242028+josegabrielguerrero26@users.noreply.github.com>
2022-11-17 09:50:07 -0500

    changes

--- chamber/js/api.js
+++ chamber/js/api.js
@@ -2,6 +2,7 @@
 import companies from '../JSON/dire.json' assert{type: 'json'};
 console.log(companies[0].ceo, companies[3].name)
 
+
 const business = companies;
 for (let i = 0; i < business.length; i++) {
     let card = document.createElement('section');
@@ -10,6 +11,7 @@ for (let i = 0; i < business.length; i++) {
     let ceo = document.createElement('p');
     let mainb = document.createElement('p');
     let ratings=document.createElement('p');
+    let email=document.createElement('p');
     let image = document.createElement('img');
 
     image.setAttribute('src', business[i].filepath);
@@ -18,6 +20,7 @@ for (let i = 0; i < business.length; i++) {
     ceo.textContent = 'CEO Name: ' + business[i].ceo;
     mainb.textContent = 'Industry: ' + business[i].mainbusiness;
     ratings.textContent='Ratings: ' + business[i].ratings;
+    email.textContent='Email: '+ business[i].website
     image.setAttribute('loading', 'lazy');
 
 
@@ -26,6 +29,10 @@ for (let i = 0; i < business.length; i++) {
     card.appendChild(ceo);
     card.appendChild(mainb);
     card.appendChild(ratings);
+    card.appendChild(email)
     card.appendChild(image);
     document.querySelector('div.cards').appendChild(card);
+  
+    
 } ;
+
