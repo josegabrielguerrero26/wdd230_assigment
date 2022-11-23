1a507fd36f2c7084f95e50b267e24ae78540d2b5
Gabe G <97242028+josegabrielguerrero26@users.noreply.github.com>
2022-11-23 18:11:05 -0500

    js fixses

--- chamber/js/adv.js
+++ chamber/js/adv.js
@@ -4,7 +4,7 @@ try {
   const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
   document.getElementById('currentdate').textContent = new Date(). toLocaleDateString('en-US', options);   
 } catch (e) {
-  alert('Error with code or your browser does not support Locate');
+  //alert('Error with code or your browser does not support Locate');
 }
 
 let lastModified = new Date(document.lastModified);
