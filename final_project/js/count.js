7287a38e9b509777a2cefecb06184f2f59c2449c
Gabe G <97242028+josegabrielguerrero26@users.noreply.github.com>
2022-12-08 12:46:07 -0500

    medium size

--- final_project/js/count.js
+++ final_project/js/count.js
@@ -4,5 +4,5 @@ let count = Number(window.localStorage.getItem("drinks"));
 if (count !== 0) {
 	drinks.textContent = `${count}`;
 } else {
-	drinks.textContent = ` Ready for your 1st recipe! `;
+	drinks.textContent = ` Send a recipe! `;
 }
