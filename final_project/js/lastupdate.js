d6402500c86a38a660d9cd5c23afeff8366ab2c0
Gabe G <97242028+josegabrielguerrero26@users.noreply.github.com>
2022-12-08 18:26:52 -0500

    done

---
+++ final_project/js/lastupdate.js
@@ -0,0 +1,13 @@
+try {
+    const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+    document.getElementById('currentdate').textContent = new Date(). toLocaleDateString('en-US', options);   
+  } catch (e) {
+    //alert('Error with code or your browser does not support Locate');
+  }
+  
+  let lastModified = new Date(document.lastModified);
+  let fullDate = lastModified.toLocaleString('en-US', {month: "2-digit", day: "2-digit", year: "numeric"});
+  let time = lastModified.toLocaleString('en-GB', {hour: "2-digit", minute: "2-digit", second: "2-digit"});
+  let dateTime = `Last Updated: ${fullDate} ${time}`;
+  document.querySelector("#lastModified").textContent= dateTime;
+  console.log(Date)
